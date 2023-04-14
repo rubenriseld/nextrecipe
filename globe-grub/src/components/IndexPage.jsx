ff499e7605dff1a22cc92689864211bc8731554d
rubenriseld <rubenriseld@outlook.com>
2023-04-14 03:52:33 +0200

    added comments describing data passing func

--- globe-grub/src/components/IndexPage.jsx
+++ globe-grub/src/components/IndexPage.jsx
@@ -6,29 +6,44 @@ import Ad from "./Ad"
 import React, {useState} from "react"
 
 export default function IndexPage(){
+
+    // bara hårdkodade titlar i default-datan här än så länge
+    // men man kanske får göra typ nåt api-anrop här sen när 
+    // vi ska kirra en sån där slumpad automatisk blablabla
+
     const [data, setData] = useState(["Mediterranean", "Scandinavian"]);
+
+    //funktionen för att sätta data till det som skickats hit
+    //från Search-komponenten
     const childToParent = (childData) => {
         setData(childData);
     }
-    
+
     return(
         <>
         
             <section className="geosearch flex-center background-secondary max-width-container">
                 <Geo></Geo>
+
+                {/* funktionen slängs in som ett attribut på Search-komponenten
+                så den finns tillgänlig där */}
+
                 <Search childToParent={childToParent}></Search>
             </section>
             <Ad/>
+{/* 
+            kört på en map här ba ifall vi skulle ha fler än 1 slumpade 
+            "rekommendationer" på startsidan
+
+            bör ju då få in data som en prop som sen kan användas för å
+            fixa massa RecipeCards inne i ResultContainer-komponenten typ */}
+
             {data.map(x =>{
                 return(
                     <ResultContainer title={x}/>
                 )
             })}
             <Ad/>
-            {/* <ResultContainer title="Hej, detta är ditt resultat:"/>
-            <Ad/>
-            <ResultContainer title="Hej, detta är ditt resultat 2:"/>
-            <Ad/> */}
         </>
     )
 }
--- globe-grub/src/components/Search.jsx
+++ globe-grub/src/components/Search.jsx
@@ -5,7 +5,21 @@ export default function Search({childToParent}){
     return(
         <div className="searchbar-container color-secondary">
             <input className="searchbar" type="search" name="queryvalue" value="search.." />
+
+                {/* lägga in resultat från api-anrop in i data 
+                och skicka det genom childToParent-funktionen här 
+                
+                kommer då finnas tillgänglig i IndexPage-komponenten
+                så vi kan mata in det till en ResultContainer på nåt sätt
+
+                lär väl behövas ett props-attribut som man slänger in i 
+                ResultContainer, och sen en funktion där i för att generera
+                ReceptKort baserat på propsen typ
+                */}
+
             <button type="button" className="search-btn color-primary" onClick={()=>childToParent(data)}>
+            
+            
                 <i className="fa-solid fa-sliders slider-icon"></i>
                {/*room for filtermenu component */}
             </button>
