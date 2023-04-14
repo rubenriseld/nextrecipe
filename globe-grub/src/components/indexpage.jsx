506670fb2b16bcda7b7d896ea01f0071474e687c
rubenriseld <rubenriseld@outlook.com>
2023-04-14 02:29:16 +0200

    moved Geo and Search component moved Geo and Search component from the combined GeoSearch component in root to IndexPage component to simplify passing data

--- globe-grub/src/components/indexpage.jsx
+++ globe-grub/src/components/indexpage.jsx
@@ -1,17 +1,19 @@
+import Geo from "./geo"
+import Search from "./search"
 import ResultContainer from "./resultcontainer"
 import Ad from "./ad"
+
 export default function IndexPage(){
     return(
         <>
-        {/* <section className="result-container max-width-container"> */}
-            {/*Main page component, showing all recipes and search result*/}
+            <section className="geosearch flex-center background-secondary max-width-container">
+                <Geo></Geo>
+                <Search></Search>
+            </section>
             <ResultContainer title="Hej, detta är ditt resultat:"/>
             <Ad/>
             <ResultContainer title="Hej, detta är ditt resultat 2:"/>
             <Ad/>
-
-        {/* </section> */}
         </>
-
     )
 }
