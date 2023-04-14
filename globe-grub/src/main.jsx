066315b788afea4a1844a5ddcd89c67007012f3c
rubenriseld <rubenriseld@outlook.com>
2023-04-14 02:55:45 +0200

    updated filepath to css-file

--- globe-grub/src/main.jsx
+++ globe-grub/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '../structure/css/react.css'
+import './react.css'
 import {
     createBrowserRouter,
     RouterProvider,
