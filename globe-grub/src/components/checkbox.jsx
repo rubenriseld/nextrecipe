28ee7f44141b124cd0c35f94ee623872b312dc4b
JockeMedKniven <JockeMedKniven@users.noreply.github.com>
2023-04-14 11:47:06 +0200

    Added line-through and grey text on check

--- globe-grub/src/components/checkbox.jsx
+++ globe-grub/src/components/checkbox.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+
 export default function Checkbox(){
+    const [isChecked, setIsChecked] = useState(false);
+    
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+      };
+
     return(
         <div class="flex checkbox-container">
-            <input type="checkbox" class="checkbox" />
-                <p class="instruction">
+            <input 
+            type="checkbox" 
+            class="checkbox" 
+            checked={isChecked} 
+            onChange={handleOnChange} />
+                <p className={`instruction ${isChecked ? "linethrough": ""}`}>
                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                  eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
          </div>
-
     )
 }
+
