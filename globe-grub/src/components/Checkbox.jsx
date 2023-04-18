fbe26514342caa29f8e29378849f2c9ee097b797
rubenriseld <rubenriseld@outlook.com>
2023-04-18 15:59:37 +0200

    fixed merge conflicts

--- globe-grub/src/components/Checkbox.jsx
+++ globe-grub/src/components/Checkbox.jsx
@@ -1,10 +1,23 @@
-export default function Checkbox(props) {
-  return (
-    <div className="flex checkbox-container">
-      <input type="checkbox" className="checkbox" />
-      <p className="instruction">
-        {props.number}. {props.step}
-      </p>
-    </div>
+import { useState } from "react";
+
+export default function Checkbox(props){
+    const [isChecked, setIsChecked] = useState(false);
+    
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+      };
+
+    return(
+        <div class="flex checkbox-container">
+            <input 
+            type="checkbox" 
+            class="checkbox" 
+            checked={isChecked} 
+            onChange={handleOnChange} />
+                <p className={`instruction ${isChecked ? "linethrough": ""}`}>
+                {props.number}. {props.step}
+            </p>
+         </div>
   );
 }
+
--- globe-grub/src/components/Geo.jsx
+++ globe-grub/src/components/Geo.jsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Geo(){
+    const [lat, setLat] = useState(null);
+    const [lng, setLng] = useState(null);
+    const [status, setStatus] = useState(null);
+   
+    const getLocation = () => {
+       if (!navigator.geolocation) {
+           setStatus("Geolocation is not supported by your browser!");
+           } 
+       else {
+                
+       setStatus("Loading...");
+           }
+           navigator.geolocation.getCurrentPosition(
+           (position) => {
+           setStatus(null);
+           setLat(position.coords.latitude);
+           setLng(position.coords.longitude);
+           },
+           () => {
+           setStatus("Unable to retrieve your location");
+               }   
+           );  
+       };
+
+
     return(
         <div className="geo-container">
-            <button type="button" className="geo-btn color-accent">
+            <NavLink to="/" type="button" className="geo-btn color-accent" onClick={getLocation}>
                 <i className="fa-solid fa-location-dot geo-icon"></i>
-            </button>
+            </NavLink>
             <p className="geo-text">Find Recipes matching your region!</p>
+            
+            {/* Only for testing */}
+            <p> 
+                {status}
+                <br/>
+                Long: {lng}
+                <br/>
+                Lat: {lat}
+            </p>
         </div>
         
     )
