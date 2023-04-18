e3fea2c2c4d7edebd0c82ddf563a7907f7defb7d
Tove <tovelyden@gmail.com>
2023-04-18 15:34:08 +0200

    Routing and data to recipepage Link to recipe page from recipe card title Route to specific recipe page /recipe/recipeid Showing recipe data on recipe page Sending instructions data to checkbox component

---
+++ globe-grub/src/components/GeoSearch.jsx
@@ -0,0 +1,9 @@
+
+export default function GeoSearch(){
+    return(
+        <section className="geosearch flex-center background-secondary max-width-container">
+            <Geo></Geo>
+            <Search></Search>
+        </section>
+    )
+}
--- globe-grub/src/components/RecipePage.jsx
+++ globe-grub/src/components/RecipePage.jsx
@@ -1,78 +1,107 @@
-import Checkbox from "./Checkbox"
-import StarRating from "./StarRating"
+import Checkbox from "./Checkbox";
+import StarRating from "./StarRating";
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-export default function RecipePage({title}) {
-    return (
+export default function RecipePage() {
+  const [recipe, setRecipe] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const location = useLocation();
+  const id = location.state;
+
+  const url = `https://api.spoonacular.com/recipes/${id}/information?&apiKey=74c179cdd6bf42fab75869c258580b05&includeNutrition=true`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setRecipe(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
     // <!-- RECIPE PAGE COMPONENT -->
     <section className="max-width-container">
-        {/* <!-- Receptnamn --> */}
-        <h1 className="recipe-title">{title}</h1>
+      {/* <!-- Receptnamn --> */}
+      <h1 className="recipe-title">{recipe.title}</h1>
 
-        <article className="recipe-container">
-            <div className="recipe-visual-container">
-                {/* <!-- Bild på maten --> */}
-                <div>
-                    <img className="recipe-image mr-4 ml-4" src="../structure/img/chickckckcn.png" />
-                </div>
-                {/* <!-- Taggar och stjärnor --> */}
-                <div className="flex flex-separate align-items-center">
-                    {/* <!-- Taggar --> */}
-                    <div className="flex tag-container">
-                        
-                        {/* <!--  ----   TAG COMPONENTS ---- --> */}
-                        {/* <!-- vi kommer göra en komponent för en enskild tagg, så på receptkorts-komponenter
+      <article className="recipe-container">
+        <div className="recipe-visual-container">
+          {/* <!-- Bild på maten --> */}
+          <div>
+            <img className="recipe-image mr-4 ml-4" src={recipe.image} />
+          </div>
+          {/* <!-- Taggar och stjärnor --> */}
+          <div className="flex flex-separate align-items-center">
+            {/* <!-- Taggar --> */}
+            <div className="flex tag-container">
+              {/* <!--  ----   TAG COMPONENTS ---- --> */}
+              {/* <!-- vi kommer göra en komponent för en enskild tagg, så på receptkorts-komponenter
                             kommer vi ha tre tagg-komponenter
                          --> */}
-                        <p className="tag color-secondary">20 min</p>
-                        <p className="tag color-secondary">asia</p>
-                        <p className="tag color-secondary">vegan</p>
-                        {/* <!-- END OF TAG COMPONENTS --> */}
-
-                    </div>
-                    
-                    {/* <!-- Stjärnor --> */}
-                    <div className="flex">
-                        {/* <!-- STAR/RATING COMPONENT --> */}
-                        <StarRating/>
-                    </div>
-
-                </div>
-            </div>
-            <div className="recipe-ingredients">
-                {/* <!-- (antalet portioner) --> */}
-                <h2 className="ingredients-title">Ingredients for 4 portions</h2>
-                {/* <!-- Ingrediensmått och ingredienser  --> */}
-                <div className="flex">
-                    {/* <!-- Ingrediensmått --> */}
-                    <div className="flex flex-column">
-                        <p className="ingredient">2 cloves</p>
-                        <p className="ingredient">2 tbsp</p>
-                        <p className="ingredient">1.7 kg</p>
-                        <p className="ingredient">50 mg</p>
-                        <p className="ingredient">6 pcs</p>
-                        <p className="ingredient">A little</p>
-                    </div>
-                    {/* <!-- Ingredienser --> */}
-                    <div className="flex flex-column">
-                        <p className="ingredient">Garlic</p>
-                        <p className="ingredient">Olive Oil</p>
-                        <p className="ingredient">Chicken</p>
-                        <p className="ingredient">Butter</p>
-                        <p className="ingredient">Potatoes</p>
-                        <p className="ingredient">Sea Salt</p>
-                    </div>
-                </div>
+              <p className="tag color-secondary">{recipe.readyInMinutes}min</p>
+              {recipe.cuisines.map((cuisineTag) => {
+                return <p className="tag color-secondary">{cuisineTag}</p>;
+              })}
+              {recipe.diets.map((dietTag) => {
+                return <p className="tag color-secondary">{dietTag}</p>;
+              })}
+              {/* <!-- END OF TAG COMPONENTS --> */}
             </div>
 
-            <div className="recipe-instructions">
-                {/* CHECKBOX COMPONENTS */}
-                <Checkbox/>
-                <Checkbox/>
-                <Checkbox/>
-                <Checkbox/>
-                <Checkbox/>
+            {/* <!-- Stjärnor --> */}
+            <div className="flex">
+              {/* <!-- STAR/RATING COMPONENT --> */}
+              <StarRating />
             </div>
-        </article>
+          </div>
+        </div>
+        <div className="recipe-ingredients">
+          {/* <!-- (antalet portioner) --> */}
+          <h2 className="ingredients-title">
+            Ingredients for {recipe.servings} portions
+          </h2>
+          {/* <!-- Ingrediensmått och ingredienser  --> */}
+          <div className="flex">
+            {/* <!-- Ingrediensmått --> */}
+            <div className="flex flex-column">
+              {recipe.extendedIngredients.map((x) => {
+                return (
+                  <p className="ingredient">
+                    {x.amount} {x.unit}
+                  </p>
+                );
+              })}
+            </div>
+            {/* <!-- Ingredienser --> */}
+            <div className="flex flex-column">
+              {recipe.extendedIngredients.map((ingredient) => {
+                return <p className="ingredient">{ingredient.name}</p>;
+              })}
+            </div>
+          </div>
+        </div>
+        {/* Instruktioner */}
+        <div className="recipe-instructions">
+          {/* CHECKBOX COMPONENTS */}
+          {recipe.analyzedInstructions.map((x) => {
+            return (
+              <>
+                {x.steps.map((y) => {
+                  return <Checkbox number={y.number} step={y.step} />;
+                })}
+              </>
+            );
+          })}
+        </div>
+      </article>
     </section>
-    )
+  );
 }
