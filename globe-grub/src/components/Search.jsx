0e9648a2967c3f713b77a13baeb070ea5b438a8e
magarnabulli <112855853+magarnabulli@users.noreply.github.com>
2023-04-19 13:47:36 +0200

    simplified hookstate works without doubleclick

--- globe-grub/src/components/Search.jsx
+++ globe-grub/src/components/Search.jsx
@@ -3,87 +3,56 @@ import { useFilterStore } from "../hooks/FilterMenu";
 import { CuisineFilters, DietFilters, IntoleranceFilters, TimeFilters, MealTypeFilters } from "./FilterItems";
 
 export default function Search({ childToParent }) {
-  // const filterUrl = async () => {
-  //   try {
-  //     const url = `https://api.spoonacular.com/recipes/complexSearch?&apiKey=${key1}&query=${searchInput}&includeIngredients=${searchInput}&addRecipeInformation=true${cuisineFilter}${dietFilter}${mealtypeFilter}${TimeFilter}${IntoleranceFilter}`;
-      
-  //     console.log(url);
-  //     const response = await fetch(url); 
-  //     const result = await response.json();
-
-  //     setRecipeData(result.results);
-  //     console.log(result)
-  //     //result.results är en lista av alla recept, dessa skickas in i childtoparent
-      
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
   const [searchInput, setSearchInput] = useState("");
   //const [recipeData, setRecipeData] = useState([]);
+  const state = useFilterStore.getState((state) => state);
+ 
+  const [cuisineFilter, setCuisineFilter] = useState("");
+
+  const [dietFilter, setDietFilter] = useState(""); 
   
-  const [count, setCount] = useState(0);
+  const [mealtypeFilter, setMealTypeFilter] = useState("");
 
-
-  const [state, getFilterState] = useState(""); //hämtar state från Hook
-  const [cuisineFilter, setCuisineFilter] = useState(""); //värde på det som ska läggas till
-  const [dietFilter, setDietFilter] = useState(""); //värde på det som ska läggas till
-  const [mealtypeFilter, setMealTypeFilter] = useState(""); //värde på det som ska läggas till
-  const [IntoleranceFilter, setIntoleranceFilter] = useState(""); //värde på det som ska läggas till
-  const [TimeFilter, setTimeFilter] = useState(""); //värde på det som ska läggas till
-  // const [countFilters, setCount] = useState(0);
+  const [IntoleranceFilter, setIntoleranceFilter] = useState("");
   
-
+  const [TimeFilter, setTimeFilter] = useState("");
+  
   const key1= "13c6c14454a748769e3611a7cf719862";
   const key2= "74c179cdd6bf42fab75869c258580b05";
   const key3= "c02162ede9394dd8bca983829213bd71";
   const key4= "85ce5287879e42978484fcf300dace17";
   const key5= "8fbd9413e79a49bfaa909d68f22e0476";
-  
+
   const AddIntoleranceFilter = (e) =>{
-    if(count <= 1){
-      getFilterState(useFilterStore.getState((state) => state.intolerances)); //hämtar värdet på cuisine taggen från hook
-      const IntoleranceFilter= (state.intolerances + e.target.value)
-      setCount(count+1);
-    }
-    else{
-   
-      setIntoleranceFilter(IntoleranceFilter + "," + e.target.value )
-      setCount(count+1);
-    }
-    console.log(count)
+      setIntoleranceFilter( state.intolerances + e.target.value)
   }
   const AddTimeFilter = (e) =>{
-    getFilterState(useFilterStore.getState((state) => state.maxReadyTime)); //hämtar värdet på cuisine taggen från hook
     setTimeFilter(state.maxReadyTime + e.target.value)
   }
   const AddMealTypeFilter = (e) =>{
-    getFilterState(useFilterStore.getState((state) => state.type)); //hämtar värdet på cuisine taggen från hook
+
     setMealTypeFilter(state.type + e.target.value)
   }
     const AddCuisinefilter = (e) =>{
-        getFilterState(useFilterStore.getState((state) => state.cuisine)); //hämtar värdet på cuisine taggen från hook
+
         setCuisineFilter(state.cuisine + e.target.value)
     }
     const AddDietFilter = (e) =>{
-      getFilterState(useFilterStore.getState((state) => state.diet)); //hämtar värdet på cuisine taggen från hook
       setDietFilter(state.diet + e.target.value)
     }
     const handleChange = (e) => {
       setSearchInput(e.target.value);
     };
-    const handleSubmit = (e) => {
-    
+    const handleSubmit = (e) => { 
     try {
-      const url = `https://api.spoonacular.com/recipes/complexSearch?&apiKey=${key2}&query=${searchInput}&includeIngredients=${searchInput}&addRecipeInformation=true${cuisineFilter}${dietFilter}${mealtypeFilter}${TimeFilter}${IntoleranceFilter}`;
+      const url = `https://api.spoonacular.com/recipes/complexSearch?&apiKey=${key4}&query=${searchInput}&includeIngredients=${searchInput}&addRecipeInformation=true${cuisineFilter}${dietFilter}${mealtypeFilter}${TimeFilter}${IntoleranceFilter}`;
       
       console.log(url);
       fetch(url)
       .then((response) => response.json())
       .then((result)=> {
         const recipeData = result.results;
-        console.log(result)
-      childToParent(recipeData); 
+        childToParent(recipeData); 
       })
   
       //result.results är en lista av alla recept, dessa skickas in i childtoparent
--- globe-grub/src/components/apitestrecipepage.jsx
+++ globe-grub/src/components/apitestrecipepage.jsx
@@ -1,6 +1,29 @@
 import {useLocation} from "react-router-dom"
 import { useState, useEffect } from "react";
 
+  
+
+  // const filterUrl = async () => {
+  //   try {
+  //     const url = `https://api.spoonacular.com/recipes/complexSearch?&apiKey=${key1}&query=${searchInput}&includeIngredients=${searchInput}&addRecipeInformation=true${cuisineFilter}${dietFilter}${mealtypeFilter}${TimeFilter}${IntoleranceFilter}`;  
+      
+  //     console.log(url);
+  //     const response = await fetch(url); 
+  //     const result = await response.json();
+
+  //     setRecipeData(result.results);
+  //     console.log(result)
+  //     //result.results är en lista av alla recept, dessa skickas in i childtoparent
+      
+  //   } catch (e) {
+  //     console.log(e);  
+  //   }
+  // };
+ 
+  
+
+
+
 export default function Recipe(){
     const [recipeData, setData] = useState(Object);
     const [cusines, setCuisines] = useState([]);
