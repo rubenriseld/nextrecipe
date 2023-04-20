5b612ddc787603fb273de6f50cb5012b558d1268
magarnabulli <112855853+magarnabulli@users.noreply.github.com>
2023-04-20 10:07:13 +0200

    Multiple filter inputs working Simplified onclick function

--- globe-grub/src/components/Search.jsx
+++ globe-grub/src/components/Search.jsx
@@ -14,7 +14,7 @@ const [searchResult, setSearchResult] = useSearchResult((state) =>
 
 //store for filter terms
   const state = useFilterStore.getState((state) => state);
- 
+  
   const [cuisineFilter, setCuisineFilter] = useState("");
 
   const [dietFilter, setDietFilter] = useState(""); 
@@ -30,45 +30,65 @@ const [searchResult, setSearchResult] = useSearchResult((state) =>
   const key3= "c02162ede9394dd8bca983829213bd71";
   const key4= "85ce5287879e42978484fcf300dace17";
   const key5= "8fbd9413e79a49bfaa909d68f22e0476";
-//   const [recipeData, setRecipeData] = useState([]);
 
+  const [showFilterMenu, setShowFilterMenu] = useState(false);
 
-  const AddIntoleranceFilter = (e) =>{
-      setIntoleranceFilter( state.intolerances + e.target.value)
-  }
-  const AddTimeFilter = (e) =>{
-    setTimeFilter(state.maxReadyTime + e.target.value)
-  }
-  const AddMealTypeFilter = (e) =>{
-
-    setMealTypeFilter(state.type + e.target.value)
-  }
-    const AddCuisinefilter = (e) =>{
-
-        setCuisineFilter(state.cuisine + e.target.value)
-    }
-    const AddDietFilter = (e) =>{
-      setDietFilter(state.diet + e.target.value)
-    }
-
-    const [showFilterMenu, setShowFilterMenu] = useState(false);
-
-    const filterUrl = async () => {
-    try {
-      const url = `https://api.spoonacular.com/recipes/complexSearch?&apiKey=${key3}&query=${searchInput}&includeIngredients=${searchInput}&addRecipeInformation=true${cuisineFilter}${dietFilter}${mealtypeFilter}${TimeFilter}${IntoleranceFilter}`;  
-      
+  const filterUrl = async () => {
+  try {
+      const url = `https://api.spoonacular.com/recipes/complexSearch?&apiKey=${key1}&query=${searchInput}&includeIngredients=${searchInput}&addRecipeInformation=true${cuisineFilter}${dietFilter}${mealtypeFilter}${TimeFilter}${IntoleranceFilter}`;     
       console.log(url);
       const response = await fetch(url); 
       const result = await response.json();
-
       setSearchResult(result.results);
       console.log(result)
-      //result.results är en lista av alla recept, dessa skickas in i childtoparent
-      
+      //result.results är en lista av alla recept, dessa skickas in i childtoparent    
     } catch (e) {
       console.log(e);  
     }
   };
+
+  const handleFilter = (e,type) =>{
+    console.log(type);
+    switch(type){
+      case "cuisine":
+        if(cuisineFilter == ""){
+          setCuisineFilter(`${state.cuisine}${e.target.value}`)     
+        }
+        else{
+          setCuisineFilter(`${cuisineFilter},${e.target.value}`)
+      };
+      break;
+      case "diet":
+        if(dietFilter == ""){
+          setDietFilter(`${state.diet}${e.target.value}`)     
+        }
+        else{
+          setDietFilter(`${dietFilter},${e.target.value}`)
+      };
+      break;
+      case "time":
+        if(TimeFilter == ""){
+          setTimeFilter(state.maxReadyTime + e.target.value);
+        }else{
+          setTimeFilter(`${TimeFilter},${e.target.value}`);
+        }
+      break;
+      case "type":
+        if(mealtypeFilter == ""){
+          setMealTypeFilter(state.type + e.target.value);
+        }else{
+          setMealTypeFilter(`${mealtypeFilter},${e.target.value}`);
+        }
+      break;
+      case "intolerances":
+        if(IntoleranceFilter == ""){
+          setIntoleranceFilter(state.intolerances + e.target.value);
+        }else{
+          setIntoleranceFilter(`${IntoleranceFilter},${e.target.value}`);
+        }
+      break;
+    }
+  }
   const handleChange = (e) => {
     setSearchInput(e.target.value);
   };
@@ -76,7 +96,6 @@ const [searchResult, setSearchResult] = useSearchResult((state) =>
     e.preventDefault();
     await filterUrl();
   };
-
   return (
     <>
     <form className="search-form" onSubmit={handleSubmit}>
@@ -89,7 +108,7 @@ const [searchResult, setSearchResult] = useSearchResult((state) =>
                 onChange={handleChange}
                 />
             <button
-                type="submit"
+                type="button"
                 className="search-btn color-primary" onClick={() => setShowFilterMenu(!showFilterMenu)}
                 >
                 <i className="fa-solid fa-sliders slider-icon"></i>
@@ -107,7 +126,7 @@ const [searchResult, setSearchResult] = useSearchResult((state) =>
                 <div className="cuisine">
                     <p className="filter-title">Cuisine</p>
                     <button className="collapse-btn">v</button>
-                    <div onClick={AddCuisinefilter}>
+                    <div onClick={(e) => handleFilter(e,'cuisine')} >
                       {CuisineFilters.map((x, index)=>{
                     return(
                     <button key={index} value={x.value}>{x.name}
@@ -118,7 +137,7 @@ const [searchResult, setSearchResult] = useSearchResult((state) =>
                 <div className="diet">
                     <p className="filter-title">Diet</p>
                     <button className="collapse-btn">v</button>
-                    <div onClick={AddDietFilter}>
+                    <div onClick={(e) => handleFilter(e,'diet')}>
                     {DietFilters.map((x, index)=>{
                   return(
                   <button key={index} value={x.value}>{x.name}
@@ -130,7 +149,7 @@ const [searchResult, setSearchResult] = useSearchResult((state) =>
                 <div className="intolerance">
                     <p className="filter-title">Intolerance</p>
                     <button className="collapse-btn">v</button>
-                      <div onClick={AddIntoleranceFilter}>
+                      <div onClick={(e) => handleFilter(e,'intolerances')}>
                         {IntoleranceFilters.map((x, index)=>{
                        return(
                       <button key={index} value={x.value}>{x.name}
@@ -142,10 +161,10 @@ const [searchResult, setSearchResult] = useSearchResult((state) =>
                 <div className="time">
                     <p className="filter-title">Time</p>
                     <button className="collapse-btn">v</button>
-                    <div onClick={AddTimeFilter}>
+                    <div onClick={(e) => handleFilter(e,'time')}>
                        {TimeFilters.map((x, index)=>{
                       return(
-                      <button key={index} value={x.value}>{x.name}
+                      <button key={index} value={x.value}>Under {x.name}
                       </button>
                        )
                      })} 
@@ -154,7 +173,7 @@ const [searchResult, setSearchResult] = useSearchResult((state) =>
                 <div className="meal">
                     <p className="filter-title">Meal</p>
                     <button className="collapse-btn">v</button>
-                    <div onClick={AddMealTypeFilter}>
+                    <div onClick={(e) => handleFilter(e,'type')}>
                       {MealTypeFilters.map((x, index)=>{
                      return(
                       <button key={index} value={x.value}>{x.name}
