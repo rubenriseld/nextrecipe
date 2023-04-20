65709437bc8cf0cd2c401472b1b784986585a9df
rubenriseld <rubenriseld@outlook.com>
2023-04-20 12:03:23 +0200

    changed design of temp ad banners

--- globe-grub/src/components/Ad.jsx
+++ globe-grub/src/components/Ad.jsx
@@ -2,9 +2,9 @@
 export default function Ad(){
     return(
         <>
-            <section className="ad max-width-container">
+            <section className="ad max-width-container background-secondary">
                 {/*Ad between recipe cards*/}
-                <h1>Annons</h1>
+                <h1 className="text-color-primary">Annons</h1>
             </section>
         </>
     )
