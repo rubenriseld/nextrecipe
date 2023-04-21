f54bea60615903be1c5ca6018c29dca2dbfec1e5
rubenriseld <rubenriseld@outlook.com>
2023-04-21 10:12:59 +0200

    fixed filtering fixed issue with latest filter button not being included in search string

--- globe-grub/src/components/FilterButton.jsx
+++ globe-grub/src/components/FilterButton.jsx
@@ -6,16 +6,13 @@ export function FilterButton(props){
     const name = props.name;
     const value = props.value;
     const type = props.type;
-    // const key = props.key;
-
 
     return(
         <button
             className={`test-btn ${isActive?"active-btn": ""}`}
-            // key={key} 
             value={value}
             data-type={type}
-            onClick={()=> setIsActive(!isActive)}>
+            onClick={()=> {setIsActive(!isActive);}}>
             {name}
         </button>
     )
