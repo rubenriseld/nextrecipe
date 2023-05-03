bf30ec18589f6a95b8ddb6d565cd6417f33c452d
rubenriseld <rubenriseld@outlook.com>
2023-05-03 15:30:37 +0200

    bool for showing slogan or not

--- globe-grub/src/components/Logo.jsx
+++ globe-grub/src/components/Logo.jsx
@@ -1,5 +1,6 @@
-export default function Logo() {
-  return (
+export default function Logo({sloganVisible}) {
+  
+    return (
     <>
       <img
         src="/images/globegrub2.png"
@@ -7,9 +8,12 @@ export default function Logo() {
         className="company-logo"
       />
       <h2 className="company-name fa-solid text-color-primary">GLOBE GRUB</h2>
-      <p className="slogan text-color-primary">
-        when you want your globe to be grubbed
-      </p>
+      {/* {sloganVisible ? */}
+        <p className="slogan text-color-primary">
+            when you want your globe to be grubbed
+        </p>
+    {/* : <></>
+      } */}
     </>
   );
 }
